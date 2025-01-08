dccd4f46dbf91d99d6c53b00069e3716c3f7be7e
oscarb <17575446+oscarb@users.noreply.github.com>
2025-01-08 14:43:14 +0100

    FIx failing test

--- app/src/test/java/se/oscarb/quicko/core/data/DefaultNoteRepositoryTest.kt
+++ app/src/test/java/se/oscarb/quicko/core/data/DefaultNoteRepositoryTest.kt
@@ -1,20 +1,36 @@
 package se.oscarb.quicko.core.data
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import se.oscarb.quicko.core.database.LocalNote
+import se.oscarb.quicko.core.database.NoteDao
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class DefaultNoteRepositoryTest {
     @Test
     fun `when new item is saved item is returned`() = runTest {
-        val repository = DefaultNoteRepository()
+        val repository = DefaultNoteRepository(FakeNoteDao())
 
         repository.add("Test note")
 
         assertEquals(repository.notes.first().size, 1)
+    }
+}
+
+private class FakeNoteDao : NoteDao {
+    private val data = mutableListOf<LocalNote>()
+
+    override fun getNotes(): Flow<List<LocalNote>> = flow {
+        emit(data)
+    }
+
+    override suspend fun insert(note: LocalNote) {
+        data.add(0, note)
     }
 }
