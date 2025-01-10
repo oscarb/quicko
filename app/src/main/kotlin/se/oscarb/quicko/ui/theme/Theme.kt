f44d1ec54da2f098acbf22c28b26ec99c1860eb3
oscarb <17575446+oscarb@users.noreply.github.com>
2025-01-10 15:55:10 +0100

    Clean up theme

--- app/src/main/kotlin/se/oscarb/quicko/ui/theme/Theme.kt
+++ app/src/main/kotlin/se/oscarb/quicko/ui/theme/Theme.kt
@@ -1,7 +1,5 @@
 package se.oscarb.quicko.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -41,11 +39,10 @@ fun QuickoTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
